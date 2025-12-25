197384a2f26b8c620b2237161c86215ee8d5753c
ImBaharie <imbaharie@gmail.com>
2025-12-25 11:19:08 +0700

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+@section('footer')
+    <footer class="main-footer">
+        <strong>Copyright &copy; {{ date('Y') }} <a href="https://adminlte.io">GenericCommerceSystem</a>.</strong>
+        All rights reserved.
+    </footer>
+@endsection
